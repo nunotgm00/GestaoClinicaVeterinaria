25df980ff42b9358f08485f8d8c2f9b812fff2a6
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-22 17:07:01 +0100

    MainWindow (Menu Inicial) WPF

---
+++ ClinicaVeterinariaWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClinicaVeterinariaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAnimals_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnClients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDoctors_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRooms_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAppointments_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
