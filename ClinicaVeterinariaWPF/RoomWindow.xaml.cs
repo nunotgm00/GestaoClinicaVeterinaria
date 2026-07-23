06b57f9937875ca04bbca6ce4a46be237086d03b
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-23 12:51:39 +0100

    RoomWindow (Frontend)

---
+++ ClinicaVeterinariaWPF/RoomWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClinicaVeterinariaWPF
+{
+    /// <summary>
+    /// Interaction logic for RoomWindow.xaml
+    /// </summary>
+    public partial class RoomWindow : Window
+    {
+        public RoomWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSeach_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGridSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
