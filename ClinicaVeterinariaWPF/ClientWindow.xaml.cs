0ae776a1bd5b581d4b63c59a3df09cbc693b89cd
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-23 11:52:17 +0100

    ClientWindow (Frontend)

---
+++ ClinicaVeterinariaWPF/ClientWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClinicaVeterinariaWPF
+{
+    /// <summary>
+    /// Interaction logic for ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddAnimal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveAnimal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGridSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
