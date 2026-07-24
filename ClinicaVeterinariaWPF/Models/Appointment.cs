1c7694e73e3ae2959ae1bb7f883ab6c4a1acc8f1
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-24 17:59:13 +0100

    Appointment Class

---
+++ ClinicaVeterinariaWPF/Models/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClinicaVeterinariaWPF.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public int AnimalId { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public int RoomId { get; set; }
+
+        public string Motive {  get; set; }
+
+        public string Treatment { get; set; }
+
+        public bool Canceled { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
