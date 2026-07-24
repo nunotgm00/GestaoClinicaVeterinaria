d06a66b6e19f3d91b324e578d060a80064396c73
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-24 18:02:48 +0100

    Room Class

---
+++ ClinicaVeterinariaWPF/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinariaWPF.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+
+        public string Type { get; set; }
+
+        public bool UnderMaintenance { get; set; }
+    }
+}
