5c84a3c593294c4ad92c2ec4c80a8a7c4bdcf7c9
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-24 17:51:22 +0100

    Animal Class

---
+++ ClinicaVeterinariaWPF/Models/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinariaWPF.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public string Breed { get; set; }
+
+        public int Age { get; set;}
+
+        public decimal Weight { get; set;}
+
+        public string Color { get; set; }
+
+        public string Sex { get; set; }
+
+        public int? ClientId { get; set; }
+    }
+}
