f0b9a62e3f5fdf69f0094c147f7a313b3d658a7d
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-24 18:06:35 +0100

    DoctorSchedule Class

---
+++ ClinicaVeterinariaWPF/Models/DoctorSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinariaWPF.Models
+{
+    internal class DoctorSchedule
+    {
+        public int Id { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public byte DayOfWeek { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
