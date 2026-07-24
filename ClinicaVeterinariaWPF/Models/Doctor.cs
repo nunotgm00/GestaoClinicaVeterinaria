2246f1513594e4bd06c7e476b789783db944526b
Nuno Melanda <nunomelanda00@gmail.com>
2026-07-24 18:01:40 +0100

    Doctor Class

---
+++ ClinicaVeterinariaWPF/Models/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinariaWPF.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Speciality { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
